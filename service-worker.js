54cf9117c4f7b675d763faed96ef5689d1961378
sofiakaur <kaursofia1497@gmail.com>
2025-08-13 07:36:02 +0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,25 +1,46 @@
-const CACHE_NAME = 'trivia-game-cache-v1';
-const ASSETS = [
-  './',
-  './index.html',
-  './style.css',
-  './script.js',
-  './favicon-96x96.png',
-  './apple-touch-icon.png',
-  './web-app-manifest-192x192.png',
-  './web-app-manifest-512x512.png'
+const CACHE_NAME = 'trivia-thief-cache-v1';
+const ASSETS_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/logo.png',
+  '/coin.png',
+  '/chest_closed.png',
+  '/badgeone.png',
+  '/badgetwo.png',
+  '/start.wav',
+  '/tick.wav',
+  '/coin.wav',
+  '/chest.wav',
+  '/Questions.json',
+  '/site.webmanifest',
+  // Add any other assets you want cached
 ];
 
-// Install Service Worker and Cache Assets
+// Install event: cache assets
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS_TO_CACHE))
   );
 });
 
-// Serve Cached Files When Offline
+// Activate event: cleanup old caches
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      )
+    )
+  );
+});
+
+// Fetch event: serve from cache, fall back to network
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(res => res || fetch(event.request))
+    caches.match(event.request, { ignoreSearch: true }).then(response => {
+      return response || fetch(event.request);
+    })
   );
 });
